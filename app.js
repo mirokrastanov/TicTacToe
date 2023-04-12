9b1d0e360b2e772a5670ee462689a3d0c0d5e169
Miroslav Krastanov <s.w.adrenaliin@gmail.com>
2023-04-12 13:08:43 +0300

    added display turn function

--- app.js
+++ app.js
@@ -3,12 +3,20 @@ const info = document.querySelector('#info');
 const emptyBoard = ('' + (", ''").repeat(8)).split(' ');
 
 function createBoard() {
-    emptyBoard.forEach((cell, i) => {
+    emptyBoard.forEach((_, i) => {
         const cellElement = document.createElement('div');
         cellElement.classList.add('square');
+        cellElement.id = i;
+        cellElement.addEventListener('click', addGo);
         gameBoard.appendChild(cellElement);
     });
 }
 
 createBoard();
 
+function addGo(e) {
+    let cell = e.target;
+    let goDisplay = document.createElement('div');
+    goDisplay.classList.add('circle');
+    cell.appendChild(goDisplay);
+}
